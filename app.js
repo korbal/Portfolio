0f302e428c287ef485af430cd2e8ecda943c1555
korbal <balint.j.korosi@gmail.com>
2022-12-28 08:03:45 +0100

    done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
 const menulinks = document.querySelectorAll('#menulinks');
+const body = document.querySelector('body');
+const moon = document.querySelector('#moon');
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('hidden');
@@ -12,4 +14,10 @@ menulinks.forEach((menulink) => {
     menu.classList.toggle('hidden');
     hamburger.classList.toggle('bg-white');
   });
+});
+
+
+moon.addEventListener('click', () => {
+  body.classList.toggle('dark');
+
 });
